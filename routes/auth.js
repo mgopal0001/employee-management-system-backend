e52e6acff391581861e8c0aeec68082a826f6c57
Madan Gopal <madan@Satyams-MacBook-Air.local>
2024-01-16 12:15:51 +0530

    small changes

--- routes/auth.js
+++ routes/auth.js
@@ -9,11 +9,9 @@ router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    console.log({ email, password });
 
     const admin = await Admin.findOne({ email });
 
-    console.log({ admin });
 
     if (!admin) {
       return res.status(401).json({
